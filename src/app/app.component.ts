696424b5e35a8f42c54cbfbcd1538eb24afd43d1
edujg81 <edujg81@gmail.com>
2024-11-19 14:58:42 +0100

    se añade carpeta assets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'proyectoAngular';
+  title = 'Proyecto de Curso Angular';
 }
