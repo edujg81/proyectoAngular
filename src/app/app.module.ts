46cd782ba397c5fea8833ba6cd07a225b8f87d4d
edujg81 <edujg81@gmail.com>
2024-11-19 14:39:56 +0100

    inicio cursoAngular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RestaurantComponent } from './restaurant/restaurant.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      RestaurantComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
